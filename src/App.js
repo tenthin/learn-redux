f3ad86cc68ab7574d9d8570986a7ea2600e719f8
Tenzin <tenthinten@gmail.com>
2022-01-21 20:15:02 +0530

    add + and - buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import {useSelector} from 'react-redux'
 
 function App() {
   const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button>+</button>
+      <button>-</button>
+      
+      {isLogged ? <h3>Valuable Informatino I shouldn't see</h3> : ''}
+      
     </div>
   );
 }
