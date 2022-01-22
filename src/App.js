a1fb8460c21768258da479033208af1f1fae566e
Tenzin <tenthinten@gmail.com>
2022-01-22 13:32:50 +0530

    increase payload by 5

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
       
       {isLogged ? <h3>Valuable Informatino I shouldn't see</h3> : ''}
